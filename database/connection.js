8e9339194419846ca61219e4e3867a4736965543
Jeferson Gonçalves <jeferson11gns@gmail.com>
2021-03-19 23:32:04 -0300

    conecção com o banco de dados

--- database/connection.js
+++ database/connection.js
@@ -8,9 +8,9 @@ client.connect(function(err) {
     if(err) {
       return console.error('could not connect to postgres', err);
     }
-    client.query('SELECT NOW() AS "theTime"', function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
+    client.query('SELECT NOW() AS "theTime"', function(error, result) {
+      if(error) {
+        return console.error('error running query', error);
       }
       console.log(result.rows[0].theTime);
       // >> output: 2018-08-23T14:02:57.117Z
