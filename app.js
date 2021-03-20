e3aeba3faf887e0bbfa50bedca42e38b887c9c9f
Jeferson Gonçalves <jeferson11gns@gmail.com>
2021-03-19 23:32:56 -0300

    Rotas organizadas e algumas implementadas

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const { request, response } = require('express')
 const express = require('express')
-const connection = require('./database/connection')
+
+const controllerOnibus = require('./controllers/controllerOnibus')
+const controllerFuncionario = require('./controllers/controllerFuncionario')
+const controllerRota = require('./controllers/controllerRota')
+const controllerCidade = require('./controllers/controllerCidade')
 
 const port = 8081
 
@@ -8,29 +13,29 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+//ROTAS
 
-//para imprimir valores da tabela use
-app.get("/", (request, response) => {
-    const query = 'SELECT * FROM Agencia;';
+//onibus
+app.get("/admin/onibus", controllerOnibus.exibiOnibus)
+app.post("/admin/onibus", controllerOnibus.addOnibus)
+app.put("/admin/onibus/atualizar/:id")
+app.delete("/admin/onibus/deletar/:id")
 
-    connection.query(query)
-        .then(res => {
-            const rows = res.rows;
+//funcionario 
+app.get("/admin/funcionario", controllerFuncionario.exibirFuncionario)
+app.post("/admin/funcionairo")
+app.put("/admin/funcionairo/atualizar/:id")
+app.delete("/admin/funcionairo/deletar/:id")
 
-            rows.map(row => {
-                console.log(`Read: ${JSON.stringify(row)}`);
-            });
-            response.send(JSON.stringify(rows))
-            //process.exit();
-            
-        })
-        .catch(err => {
-            console.log(err);
-        });
-        
-   
-})
+//rota
+app.get("/admin/rota", controllerRota.exibirRotas)
+app.post("/admin/rota", controllerRota.addRota)
+app.put("/admin/rota/atualizar/:id")
+app.delete("/admin/rota/deletar/:id")
 
+//cidade
+app.get("/admin/cidade", controllerCidade.exibirCidade)
+app.post("/admin/cidade", controllerCidade.addCidade)
 
 app.listen(port, ()=>{
     console.log(`Servidor conectado na porta ${port}`)
